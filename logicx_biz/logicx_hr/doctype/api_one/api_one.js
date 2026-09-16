ce6e08d49ea9d5550924268c2a69c8fb3a841c77
AshokKumar-RV <ashok@logicx.in>
2026-09-16 19:24:07 +0530

    #38 minor

--- logicx_biz/logicx_hr/doctype/api_one/api_one.js
+++ logicx_biz/logicx_hr/doctype/api_one/api_one.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on("API One", {
 
 	refresh(frm) {
-		show_endpoint(frm);
 		if (!frm.is_new()) {
 			frm.add_custom_button(__("View Logs"), () => {
 				frappe.set_route("List", "API One Log", { api_path: frm.doc.api_path });
@@ -13,25 +12,6 @@ frappe.ui.form.on("API One", {
 		if (frm.doc.api_path) {
 			frm.set_value("api_path", frm.doc.api_path.trim());
 		}
-		show_endpoint(frm);
-	},
-
-	request_method(frm) {
-		show_endpoint(frm);
-	},
-
-	enabled(frm) {
-		show_endpoint(frm);
 	},
 
 });
-
-function show_endpoint(frm) {
-	if (!frm.doc.api_path) {
-		frm.set_intro("");
-		return;
-	}
-	const endpoint = `${frm.doc.request_method || "GET"} /api/method/${frm.doc.api_path}`;
-	const state = frm.doc.enabled ? "" : ` (${__("disabled")})`;
-	frm.set_intro(__("Endpoint: {0}{1}", [`<code>${endpoint}</code>`, state]), frm.doc.enabled ? "blue" : "orange");
-}
